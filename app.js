dc1e8eff7edc8127c653f13b505dc37fa547c06a
Stacey <stacey6105@gmail.com>
2022-10-28 14:55:26 +0100

    Add header and footer styles, add nav routes

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
     res.render('home', {pageTitle: "Home"});
 });
+app.get("/about", function(req, res) {
+    res.render('about', {pageTitle: "About"});
+})
+app.get("/contact", function(req, res) {
+    res.render('contact', {pageTitle: "Contact"});
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
