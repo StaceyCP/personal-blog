7907727301785832c58c17e2934e1663e5e4811f
Stacey <stacey6105@gmail.com>
2022-10-30 10:34:08 +0000

    Post's update and are added to the home page and the posts page

--- app.js
+++ app.js
@@ -7,15 +7,33 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+let posts = [];
+
 app.get("/", function(req, res) {
-    res.render('home', {pageTitle: "Home"});
+    res.render('home', {pageTitle: "Home", posts: posts});
 });
 app.get("/about", function(req, res) {
     res.render('about', {pageTitle: "About"});
-})
+});
 app.get("/contact", function(req, res) {
     res.render('contact', {pageTitle: "Contact"});
-})
+});
+app.get("/compose", function(req, res) {
+    res.render('compose', {pageTitle: "Compose"});
+});
+app.get("/posts", function(req, res) {
+    res.render('posts', {pageTitle: "Posts", posts: posts});
+});
+
+
+
+app.post("/", function(req, res) {
+    let postTitle = req.body.postTitle
+    let postContent = req.body.postContent
+    posts.push({postTitle, postContent});
+    console.log(posts);
+    res.redirect("/");
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
